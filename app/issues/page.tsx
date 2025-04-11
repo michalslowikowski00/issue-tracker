79ace00a18085125e119f2fbab75bd085c4b547b
michalslowikowski <michalslowikowski00@gmail.com>
2025-04-11 13:06:43 +0200

    add new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
 import React from 'react';
 
 const IssuesPage = () => {
   return (
     <div>
-      <Button>DEATH METAL </Button>
+      <Button>
+        <Link href="issues/new">New issue</Link>
+      </Button>
     </div>
   );
 };
