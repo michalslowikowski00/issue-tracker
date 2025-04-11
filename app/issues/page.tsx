7713a1f6cf0603fe562ccec24929382cd8a87726
michalslowikowski <michalslowikowski00@gmail.com>
2025-04-11 12:22:18 +0200

    setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { Button } from '@radix-ui/themes';
+import React from 'react';
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Button>DEATH METAL </Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
