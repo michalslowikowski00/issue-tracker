27f73057a64871b50848c9e07a80d642594a7b79
michalslowikowski <michalslowikowski00@gmail.com>
2025-04-11 12:44:13 +0200

    fix

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-
-const IssuesPage = () => {
-  return <div>IssuesPage</div>;
-=======
 import { Button } from '@radix-ui/themes';
 import React from 'react';
 
@@ -13,7 +7,6 @@ const IssuesPage = () => {
       <Button>DEATH METAL </Button>
     </div>
   );
->>>>>>> creating-issues
 };
 
 export default IssuesPage;
