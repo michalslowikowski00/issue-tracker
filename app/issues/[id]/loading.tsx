5422242aa221a4c9b33224a03ebd6fc46955c384
michalslowikowski <michalslowikowski00@gmail.com>
2025-04-14 16:55:09 +0200

    loging skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Box, Card, Flex } from '@radix-ui/themes';
+import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 const LoadingDeatilsIssuePage = () => {
   return (
-    <div>LoadingDeatilsIssuePage</div>
-  )
-}
+    <Box>
+      <Skeleton className="max-w-xl" />
+      <Flex gap="1" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
+};
 
-export default LoadingDeatilsIssuePage
+export default LoadingDeatilsIssuePage;
